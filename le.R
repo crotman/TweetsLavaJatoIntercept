ed2df34678042f680cac99279e539ea234161e50
Bruno Mauricio Rodrigues Crotman <bruno.crotman@epe.gov.br>
2019-07-02 08:50:26 -0300

    Atualizacao

--- le.R
+++ le.R
@@ -2,10 +2,6 @@ library(rtweet)
 library(tidyverse)
 
 twitter_token <- create_token(app = "phd_crotman", 
-                              consumer_key = "x921Syc05Pj3mULYpZPHKxIGj", 
-                              consumer_secret = "YBGJczZLX8ysJZrlILqyeBBziQoPslWcILK0yjdUZIoFLtOhhd",
-                              access_token = "1020284091139330048-IOBDfLI2cDCyBzQqVFAUbgcmWgK1Nu",
-                              access_secret = "xHXSvQXP5T0EZAJcasZlBDnlfUqTMeez5MIbkov2X3SLd"
 )
 
 rt <- search_tweets(
